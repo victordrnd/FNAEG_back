6c06df42db52bf67795db52104dcf8f6cc25cc0e
Victor Durand <vic20016@gmail.com>
2020-01-09 15:43:59 +0100

    feat: create users table

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,14 +1,10 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
-use JWTAuth;
-
 class AuthController extends Controller
 {
   public function login(Request $request)
@@ -18,21 +14,19 @@ class AuthController extends Controller
       'password' => 'required|string',
     ]);
     if ($validator->fails()) {
-      return Controller::responseJson(422, 'Une erreur est survenue',$validator->errors());
-      //return $this->validationError($validator);
+      return Controller::responseJson(422, 'Une erreur est survenue', $validator->errors());
     }
     $credentials = $request->only(['email', 'password']);
-    if (!$token = auth()->attempt($credentials)) {
+    if (!$token = auth()->setTTL(525600)->attempt($credentials)) {
       return $this->responseJson(Controller::$HTTP_UNAUTHORIZED, 'Les identifiants sont incorrects');
     }
     $data =  [
-      'user' => JWTAuth::user(),
+      'user' => auth()->user(),
       'token' => $token,
+      'expire' => auth()->factory()->getTTL() * 60
     ];
     return $this->responseJson(200, 'Identifiants valides', $data);
   }
-
-
   public function signup(Request $request)
   {
     $validator  = Validator::make($request->all(), [
@@ -43,17 +37,16 @@ class AuthController extends Controller
       'phone' => 'string'
     ]);
     if ($validator->fails()) {
-      return Controller::responseJson(422, 'Certains champs sont manquants',$validator->errors());
+      return Controller::responseJson(422, 'Certains champs sont manquants', $validator->errors());
     }
     $password = $request->password;
     $request->merge(['password' => Hash::make($password)]);
     $user = User::create($request->all());
-
     $credentials = [
       'email' => $request->email,
       'password' =>  $password
     ];
-    if (!$token = auth()->attempt($credentials)) {
+    if (!$token = auth()->setTTL(525600)->attempt($credentials)) {
       return $this->responseJson(Controller::$HTTP_NOK, 'Les identifiants sont incorrects');
     }
     $data =  [
@@ -62,13 +55,12 @@ class AuthController extends Controller
     ];
     return $this->responseJson(Controller::$HTTP_OK, "L'enregistrement a réussi", $data);
   }
-
-
   public function getCurrentUser()
   {
     $data =  [
       'user' => auth()->user()
     ];
-    return Controller::responseJson(200, "L'utilisateur a été retourné" , $data);
+    return Controller::responseJson(200, "L'utilisateur a été retourné", $data);
   }
 }
+
